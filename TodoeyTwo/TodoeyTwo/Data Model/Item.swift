1db5b7a569df4b9d1f7a014d9d1d73703290cccf
Even Lauvrak <evenwienenga@gmail.com>
2018-03-21 19:27:49 +0100

    Convert to MVC design pattern

---
+++ TodoeyTwo/TodoeyTwo/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  TodoeyTwo
+//
+//  Created by Even Lauvrak on 21/03/2018.
+//  Copyright © 2018 Even Lauvrak. All rights reserved.
+//
+
+import UIKit
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+    
+}
